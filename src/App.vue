5e7e1b89f3a7bee3338234ef6881decf4bf6980f
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-05-16 19:25:37 +0200

    correction des liens vers les images

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
     <search/>
 
     <div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(22rem,1fr))] max-w-7xl m-auto">
-      <card image="public/maison1.jpg"
+      <card image="/public/maison1.jpg"
             price="2,700"
             nom="Beverly Springfield"
             adress="2821 Lake Sevilla, Palm Harbor, TX "
@@ -13,7 +13,7 @@
             bathroom="2"
             taille="6x7.5"/>
 
-      <card image="public/maison2.jpg"
+      <card image="/public/maison2.jpg"
             price="2,095"
             nom="Palm Harbor"
             adress="2699 Green Valley, Highland Lake"
@@ -21,7 +21,7 @@
             bathroom="2"
             taille="6x7"/>
 
-      <card image="public/maison3.jpg"
+      <card image="/public/maison3.jpg"
             price="4,550"
             nom="Faulkner Ave"
             adress="909 Woodland St, Michigan, IN"
@@ -29,7 +29,7 @@
             bathroom="3"
             taille="8x10"/>
 
-      <card image="public/maison4.jpg"
+      <card image="/public/maison4.jpg"
             price="4,650"
             nom="St. Christal"
             adress="210 US Highway, HighlandLake, FL"
@@ -37,7 +37,7 @@
             bathroom="2"
             taille="6x7.5"/>
 
-      <card image="public/maison5.jpg"
+      <card image="/public/maison5.jpg"
             price="3,100"
             nom="Cover Red"
             adress="243 CurlewRoad, PalmHarbor, TX"
@@ -45,7 +45,7 @@
             bathroom="2"
             taille="6x7.5"/>
 
-      <card image="public/maison6.jpg"
+      <card image="/public/maison6.jpg"
             price="4,800"
             nom="Tarpon Bay"
             adress="103 Lake Shores, Michigan, IN"
